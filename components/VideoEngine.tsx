065e68bf8a59ab049b88c0ecd2489118a8986d83
supertlj <supertlj@msn.com>
2026-05-06 21:26:16 -0500

    fix: 将视频容器尺寸改为 100% 以适配旋转后的移动端布局

--- components/VideoEngine.tsx
+++ components/VideoEngine.tsx
@@ -79,8 +79,8 @@ export const VideoEngine: React.FC<VideoEngineProps> = ({ onVideoEnd, onTimeUpda
       <style jsx>{`
         .video-container {
           position: relative;
-          width: 100vw;
-          height: 100vh;
+          width: 100%;
+          height: 100%;
           background: black;
           overflow: hidden;
         }
